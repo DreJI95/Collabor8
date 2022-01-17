79ace79347a6a28e01f3eac12b61f6fd0daec0ad
powerar <arpower@bell.net>
2022-01-16 21:02:11 -0500

    fixed syntax errors when server starts

--- server/schema/modelResolvers/teamResolvers.js
+++ server/schema/modelResolvers/teamResolvers.js
@@ -1,50 +1,52 @@
-const {Team} = require('../../models');
+const { Team } = require('../../models');
 
 const teamResolvers = {
+  //update with selects to exclude versions, _ids, etc.
 
-    //update with selects to exclude versions, _ids, etc.
+  /*Queries*/
+  getTeam: async function (parent, { _id }) {
+    return await Team.findById(_id).populate('members').populate('projects'); //need to test if the populate will include the professionals and projects details in results
+  },
 
-    /*Queries*/
-    getTeam: async function(parent, {_id}){
-        return await Team.findById(_id).populate('members').populate('projects'); //need to test if the populate will include the professionals and projects details in results
-    },
+  getTeams: async function () {
+    return await Team.find({}).populate('members').populate('projects');
+  },
 
-    getTeams: async function(){
-        return await Team.find({}).populate('members').populate('projects');
-    },
+  /*Mutations*/
+  createTeam: async function (parent, team) {
+    /* teams should import the model values for [professionals(ID)],[projects(ID)] & teamName*/
+    return await Team.create(team)
+      .populate('professionals')
+      .populate('projects');
+  },
 
-    /*Mutations*/
-    createTeam: async function(parent, team){
+  // //=======================Need to test if I can add & remove in bulk or if i need to do foreach ================
+  //     addTeamProfessional(parent, {_id, professionals}) {
+  //         return await Team.findByIdAndUpdate( _id, {$push: {professionals}},{new: true}).populate('professionals').populate('projects');
+  //     },
 
-        /* teams should import the model values for [professionals(ID)],[projects(ID)] & teamName*/
-        return await Team.create(team).populate('professionals').populate('projects');
-    },
+  //     addTeamProject(parent, {_id, projects}) {
+  //         return await Team.findByIdAndUpdate( _id, {$push: {projects}},{new: true}).populate('professionals').populate('projects');
+  //     },
 
-    //=======================Need to test if I can add & remove in bulk or if i need to do foreach ================
-        addTeamProfessional(parent, {_id, professionals}) {
-            return await Team.findByIdAndUpdate( _id, {$push: {professionals}},{new: true}).populate('professionals').populate('projects');
-        },
+  //     removeTeamProfessional(parent, {_id, professionals}) {
+  //         return await Team.findByIdAndUpdate( _id, {$pull: {professionals}},{new: true}).populate('professionals').populate('projects');
+  //     },
 
-        addTeamProject(parent, {_id, projects}) {
-            return await Team.findByIdAndUpdate( _id, {$push: {projects}},{new: true}).populate('professionals').populate('projects');
-        },
+  //     removeTeamProject(parent, {_id, projects}) {
+  //         return await Team.findByIdAndUpdate( _id, {$pull: {projects}},{new: true}).populate('professionals').populate('projects');
+  //     },
+  // //========================//
 
-        removeTeamProfessional(parent, {_id, professionals}) {
-            return await Team.findByIdAndUpdate( _id, {$pull: {professionals}},{new: true}).populate('professionals').populate('projects');
-        },
+  updateTeamName: async function (parent, { _id, teamName }) {
+    return await Team.findByIdAndUpdate(_id, teamName, { new: true })
+      .populate('professionals')
+      .populate('projects');
+  },
 
-        removeTeamProject(parent, {_id, projects}) {
-            return await Team.findByIdAndUpdate( _id, {$pull: {projects}},{new: true}).populate('professionals').populate('projects');
-        },
-    //========================//
-
-    updateTeamName: async function(parent, {_id, teamName}){
-        return await Team.findByIdAndUpdate( _id, teamName,{new: true}).populate('professionals').populate('projects');
-    },
-
-    deleteTeam: async function(parent, {_id}){
-        return await Team.findOneAndDelete(_id);
-    }
-}
+  deleteTeam: async function (parent, { _id }) {
+    return await Team.findOneAndDelete(_id);
+  },
+};
 
 module.exports = teamResolvers;
--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -3,91 +3,99 @@ const { gql } = require('apollo-server-express');
 
 //define the schema of the database to 'shape' our data
 const typeDefs = gql`
+  type Skill {
+    _id: ID
+    skill: String
+  }
 
-    type Skill {
-        _id: ID
-        skill: String
-    }
+  type Professional {
+    _id: ID
+    email: String
+    password: String
+    firstName: String
+    lastName: String
+    url: String
+    strengths: [String]
+    weaknesses: [String]
+    skills: [Skill]
+  }
 
-    type Professional {
-        _id: ID
-        email: String
-        password: String
-        firstName: String
-        lastName: String
-        url: String
-        strengths: [String]
-        weaknesses: [String]
-        skills: [Skill]
-    }
+  type AuthedPro {
+    token: ID!
+    professional: Professional
+  }
+  type Team {
+    _id: ID
+    teamMembers: [String!]!
+  }
 
-    type AuthedPro {
-        token: ID!
-        professional: Professional
-    }
-    type Team {
-        _id: ID
-        teamMembers: [String!]!
-    }
+  input ProfessionalData {
+    email: String
+    password: String
+    firstName: String
+    lastName: String
+    url: String
+    strengths: [String]
+    weaknesses: [String]
+    skills: [ID]
+  }
 
-    input ProfessionalData {
-        email: String
-        password: String
-        firstName: String
-        lastName: String
-        url: String
-        strengths: [String]
-        weaknesses: [String]
-        skills: [ID]
-    }
+  type Project {
+    projectTitle: String!
+    visibility: String!
+    description: String!
+    image: String
+    repositoryLink: String
+    deployedLink: String
+    status: String
+    startDate: String
+    completionDate: String
+  }
 
-   type Project {
-        projectTitle: String!
-        visibility: String!
-        description: String!
-        image: String
-        repositoryLink: String
-        deployedLink: String
-        status: String
-        startDate: Date
-        completionDate: Date
-    }
+  type Team {
+    members: [Professional]
+    projects: [Project]
+    teamName: String
+    startDate: String
+  }
 
-    type Team {
-        members: [Professional]
-        projects: [Project]
-        teamName: String
-        startDate: Date
-    }
+  type Query {
+    getProfessionals: [Professional]
+    getProfessional(_id: ID!): Professional
+    getSkills: [Skill]
+    getSkill(_id: ID!): Skill
+    getProjects: [Project]
+    getProject(_id: ID!): Project
+    getTeam(_id: ID!): Team
+    getTeams: [Team]
+  }
 
-    type Query {
-        getProfessionals: [Professional]
-        getProfessional(_id: ID!): Professional
-        getSkills: [Skill]
-        getSkill(_id: ID!): Skill
-        getProjects: [Project]
-        getProject(_id: ID!): Project
-        getTeam(_id: ID!): Team
-        getTeams: [Team]
-    }
-
-    type Mutation {
-        login(email: String!, password: String!): AuthedPro
-        createProfessional(professional: ProfessionalData!): AuthedPro
-        deleteProfessional(_id: ID!): Professional
-        updateProfessional(_id: ID!, professional: ProfessionalData!): Professional
-        addSkillToPro(idPro: ID!, idSkill: ID!): Professional
-        removeSkillFromPro(idPro: ID!, idSkill: ID!): Professional
-        createSkill(skill: String!): Skill
-        deleteSkill(_id: ID!): Skill
-        createTeam(professionals: String!, projects: String!, teamName: String): Team
-        addTeamProfessional(_id: ID!, professionals: ID!): Team
-        addTeamProject(_id: ID!, projects: ID!): Team
-        removeTeamProfessional(_id: ID!, professionals: ID!): Team
-        removeTeamProject(_id: ID!, projects: ID!): Team
-        updateTeamName(_id: ID!, teamName: String): Team
-        deleteTeam(_id: ID!): Team
-    }
+  type Mutation {
+    login(email: String!, password: String!): AuthedPro
+    createProfessional(professional: ProfessionalData!): AuthedPro
+    deleteProfessional(_id: ID!): Professional
+    updateProfessional(_id: ID!, professional: ProfessionalData!): Professional
+    addSkillToPro(idPro: ID!, idSkill: ID!): Professional
+    removeSkillFromPro(idPro: ID!, idSkill: ID!): Professional
+    createSkill(skill: String!): Skill
+    deleteSkill(_id: ID!): Skill
+    createTeam(
+      professionals: String!
+      projects: String!
+      teamName: String
+    ): Team
+    addTeamProfessional(_id: ID!, professionals: ID!): Team
+    addTeamProject(_id: ID!, projects: ID!): Team
+    removeTeamProfessional(_id: ID!, professionals: ID!): Team
+    removeTeamProject(_id: ID!, projects: ID!): Team
+    updateTeamName(_id: ID!, teamName: String): Team
+    deleteTeam(_id: ID!): Team
+    createProject(
+      projectTitle: String!
+      visibility: String!
+      description: String!
+    ): Project
+  }
 `;
 
 module.exports = typeDefs;
